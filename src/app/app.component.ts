4fba3e5b07afd4e9789633da57dc0ac72b7f3ff0
Yannick Simard <yannick.simard94@gmail.com>
2023-03-18 15:21:10 -0400

    Ajout de la composante todo-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>To-do List App</h1>
+    <app-todo-list></app-todo-list>
+  `
 })
 export class AppComponent {
-  title = 'todo-list-app';
+  title = 'To-Do List App';
 }
